b27f7bb6beddfd82dc9ab79f7accfee39b106e8f
John Helveston <john.helveston@gmail.com>
2021-08-18 16:44:17 -0400

    Update 2-exploreData.R

--- code/2-exploreData.R
+++ code/2-exploreData.R
@@ -20,11 +20,11 @@ head(data)
 # "accelTime"   = 0 to 60 mph acceleration time in seconds (6, 7, 8)
 # "powertrain"  = Indicates if the car is electric or gas
 
-# Look at unique values in data:
-data %>% distinct(price)
-data %>% distinct(fuelEconomy)
-data %>% distinct(accelTime)
-data %>% distinct(powertrain)
+# Look at counts of values in data:
+data %>% count(price)
+data %>% count(fuelEconomy)
+data %>% count(accelTime)
+data %>% count(powertrain)
 
 # Count number of times each alternative was chosen:
 data %>% count(altID, choice)
@@ -32,17 +32,21 @@ data %>% count(altID, choice)
 # Check if any respondents made the same choice for all questions:
 data %>% 
     filter(choice == 1) %>%
-    count(respID, altID) %>%
+    count(respID, altID) %>% 
     filter(n == 8)
 
-# Visualize how many times each price level was chosen:
+# Check respondent 174:
 data %>% 
-    count(price, choice)
+    filter(respID == 174)
+
+# Visualize how many times each price level was chosen:
+data %>% count(price, choice)
 data %>%
     ggplot() + 
     geom_col(aes(x = as.factor(price), y = choice))
 
 # Visualize how many times electric cars were chosen compared to gasoline cars:
+data %>% count(powertrain, choice)
 data %>%
     ggplot() + 
     geom_col(aes(x = as.factor(powertrain), y = choice))
