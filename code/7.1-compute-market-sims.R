864ff793be1d06c3841212c7c930120f48ebf011
emse-p4a-gwu <john.helveston@gmail.com>
2025-09-28 11:45:43 -0400

    sims

--- code/7.1-compute-market-sims.R
+++ code/7.1-compute-market-sims.R
@@ -6,60 +6,60 @@ library(here)
 library(logitr)
 
 # Load estimated models
-load(here("models", "mnl.RData"))
+load(here("models", "model_mnl.RData"))
 
 # -----------------------------------------------------------------------------
-# Single market simulation using the linear model
+# Single market simulation using the mnl model
 
-summary(mnl_linear)
+summary(model_mnl)
 
 # Create a set of alternatives for which to simulate shares
 baseline <- data.frame(
-    altID       = c(1, 2, 3), 
-    obsID       = c(1, 1, 1),
-    price       = c(15, 25, 21),
-    fuelEconomy = c(20, 100, 40),
-    accelTime   = c(8, 6, 7),
-    powertrain_Electric  = c(0, 1, 0)
+  altID = c(1, 2, 3),
+  obsID = c(1, 1, 1),
+  price = c(15, 25, 21),
+  fuelEconomy = c(20, 100, 40),
+  accelTime = c(8, 6, 7),
+  powertrainelectric = c(0, 1, 0)
 )
 
 # Columns are attributes, rows are alternatives
 baseline
 
 # Use the predict() function to compute the probabilities
-sim_mnl_linear <- predict(
-    mnl_linear,
-    newdata = baseline, 
-    obsID = 'obsID', 
-    level = 0.95,
-    interval = 'confidence',
-    returnData = TRUE # This returns your data along with predicted values
+sim_mnl <- predict(
+  model_mnl,
+  newdata = baseline,
+  obsID = 'obsID',
+  level = 0.95,
+  interval = 'confidence',
+  returnData = TRUE # This returns your data along with predicted values
 )
 
-sim_mnl_linear
+sim_mnl
 
 # -----------------------------------------------------------------------------
-# Multiple simulations using the linear model
+# Multiple simulations using the mnl model
 
 # Read in market scenarios
 scenarios <- read_csv(here('data', 'scenarios.csv'))
 head(scenarios)
 
 # Use the predict() function to compute the probabilities
-sim_mnl_linear_multi <- predict(
-    mnl_linear,
-    newdata = scenarios, 
-    obsID = 'obsID', 
-    level = 0.95,
-    interval = 'confidence',
-    returnData = TRUE
+sim_mnl_multi <- predict(
+  model_mnl,
+  newdata = scenarios,
+  obsID = 'obsID',
+  level = 0.95,
+  interval = 'confidence',
+  returnData = TRUE
 )
 
-head(sim_mnl_linear_multi)
+head(sim_mnl_multi)
 
 # Save simulations
 save(
-    sim_mnl_linear,
-    sim_mnl_linear_multi,
-    file = here("sims", "mnl_linear.RData")
+  sim_mnl,
+  sim_mnl_multi,
+  file = here("sims", "mnl.RData")
 )
--- code/7.2-plot-market-sims.R
+++ code/7.2-plot-market-sims.R
@@ -5,41 +5,48 @@ library(tidyverse)
 library(here)
 
 # Load simulation results
-load(here("sims", "mnl_linear.RData"))
+load(here("sims", "mnl.RData"))
 
-# Bar plot of probabilities for single market simulation (with 95% CI) 
-sim_mnl_linear %>% 
-    mutate(label = c("Conventional", "Electric", "Hybrid")) %>% 
-    ggplot(aes(
-        x = label, y = predicted_prob, 
-        ymin = predicted_prob_lower, ymax = predicted_prob_upper)) +
-    geom_col(fill = "grey", width = 0.6) +
-    geom_errorbar(width = 0.3) +
-    scale_y_continuous(limits = c(0, 1), labels = scales::percent) +
-    labs(x = 'Alternative', y = 'Market Share') +
-    theme_bw()
-
-# Save plot
-ggsave(
-    filename = here('figs', 'sim.png'), 
-    width = 4, height = 3.5
-)
-
-# Bar plot of probabilities for multiple market simulations (with 95% CI) 
-sim_mnl_linear_multi %>%
-    ggplot(aes(
-        x = as.factor(altID), y = predicted_prob, 
-        ymin = predicted_prob_lower, ymax = predicted_prob_upper)) +
-    geom_col(fill = "grey", width = 0.6) +
-    geom_errorbar(width = 0.3) +
-    facet_wrap(~obsID) +
-    scale_y_continuous(limits = c(0, 1), labels = scales::percent) +
-    labs(x = 'Alternative', y = 'Market Share') +
-    theme_bw()
+# Bar plot of probabilities for single market simulation (with 95% CI)
+sim_mnl %>%
+  mutate(label = c("Conventional", "Electric", "Hybrid")) %>%
+  ggplot(aes(
+    x = label,
+    y = predicted_prob,
+    ymin = predicted_prob_lower,
+    ymax = predicted_prob_upper
+  )) +
+  geom_col(fill = "grey", width = 0.6) +
+  geom_errorbar(width = 0.3) +
+  scale_y_continuous(limits = c(0, 1), labels = scales::percent) +
+  labs(x = 'Alternative', y = 'Market Share') +
+  theme_bw()
 
 # Save plot
 ggsave(
-    filename = here('figs', 'sim_multi.png'), 
-    width = 8, height = 7
+  filename = here('figs', 'sim_mnl.png'),
+  width = 4,
+  height = 3.5
 )
 
+# Bar plot of probabilities for multiple market simulations (with 95% CI)
+sim_mnl_multi %>%
+  ggplot(aes(
+    x = as.factor(altID),
+    y = predicted_prob,
+    ymin = predicted_prob_lower,
+    ymax = predicted_prob_upper
+  )) +
+  geom_col(fill = "grey", width = 0.6) +
+  geom_errorbar(width = 0.3) +
+  facet_wrap(~obsID) +
+  scale_y_continuous(limits = c(0, 1), labels = scales::percent) +
+  labs(x = 'Alternative', y = 'Market Share') +
+  theme_bw()
+
+# Save plot
+ggsave(
+  filename = here('figs', 'sim_multi.png'),
+  width = 8,
+  height = 7
+)
